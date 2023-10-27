a8a97a920ec6de0a76712fc521aeb48417b8eca3
tony_yang <tongyunyang@tudelft.nl>
2023-10-27 02:27:17 +0200

    Very good results.

--- modules/kdd_model.py
+++ modules/kdd_model.py
@@ -295,8 +295,8 @@ class TSTransformerEncoder(nn.Module):
         self.embedding = embedding
 
         # Below are configurations for the convolution layer
-        kernel_size = 5
-        stride = 1
+        kernel_size = 20
+        stride = 10
         dilation = 1
         padding = 0
 
@@ -535,8 +535,8 @@ class TSTransformerEncoderClassiregressor(nn.Module):
         self.embedding = embedding
 
         # Below are configurations for the convolution layer
-        kernel_size = 5
-        stride = 1
+        kernel_size = 20
+        stride = 10
         dilation = 1
         padding = 0
 
